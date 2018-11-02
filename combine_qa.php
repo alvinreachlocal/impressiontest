718fd07b8f2627873978a1d666cd42f667eecbf6
Alvin Reyes <alvin.reyes@reachlocal.com>
2018-11-02 10:36:19 -0700

    Create combine_qa.php

---
+++ combine_qa.php
@@ -0,0 +1,184 @@
+
+
+<html>
+<head>
+
+<script type="text/javascript" src="//cdn-qa.rlets.com/capture_configs/fc6/2c2/8f3/f38481285c3b3fe1329dba8.js" async="async"></script>
+
+
+
+
+<!--<script type="text/javascript" src="//cdn.rlets.com/capture_static/mms/mms.js" async="async"></script>-->
+
+<!--<script type="text/javascript" src="//cdn.rlets.com/capture_configs/176/99e/e32/24346ffa39bd01419da92dd.js" ></script>-->
+
+
+<!--<script type="text/javascript" src="//cdn-qa.rlets.com/capture_configs/1bc/b88/3d2/8984f7ba324b12635c7acbb.js" ></script> -->
+
+
+</head>
+
+
+<body>
+
+<div id=phone_replacement>8182740260</div>
+<p></p>
+
+<a href="/iframe/combine_iframe.php"> IFRAME TRACKING </a>
+
+<div id=string_replacement>Jeff1</div>
+<p></p>
+<a href="cvt.php"> CVT </a>
+<p></p>
+
+<a href="mailto:alvin.m.reyes@gmail.com"> Email Us </a>
+
+<p></p>
+
+818.274.0260<Br>
+
+
+<p>
+
+
+
+
+
+
+
+
+
+<form id="mini_form3" action="#" method="post">
+
+Name: <input id="fullname" type="text" name="captcha-1" value="" class="input-text" autocomplete="off"> <br>
+homephone:<input id="homephone" type="text" name="homephone" value="" class="input-text" autocomplete="off"> <br>
+Message:<input id="message" type="text" name="q2" value="" class="input-text" autocomplete="off"> <br>
+<button id="button44" type="submit" title="For Product">submit your form</button>
+</form>
+
+<p></p><p></p><p></p><p></p><p></p>
+
+
+<form id="mini_form3" action="#" method="post">
+
+Name: <input id="fullname" type="text" name="captcha-1" value="" class="input-text" autocomplete="off"> <br>
+homephonenumber:<input id="homephonenumber" type="text" name="homephonenumber" value="" class="input-text" autocomplete="off"> <br>
+Message:<input id="message" type="text" name="q2" value="" class="input-text" autocomplete="off"> <br>
+<button id="button44" type="submit" title="For Product">submit your form</button>
+</form>
+
+<p></p><p></p><p></p><p></p><p></p>
+
+<form id="mini_form3" action="#" method="post">
+
+Name: <input id="fullname" type="text" name="captcha-1" value="" class="input-text" autocomplete="off"> <br>
+mobilephone:<input id="mobilephone" type="text" name="mobilephone" value="" class="input-text" autocomplete="off"> <br>
+Message:<input id="message" type="text" name="q2" value="" class="input-text" autocomplete="off"> <br>
+<button id="button44" type="submit" title="For Product">submit your form</button>
+</form>
+
+<p></p><p></p><p></p><p></p><p></p>
+
+<form id="mini_form3" action="#" method="post">
+
+Name: <input id="fullname" type="text" name="captcha-1" value="" class="input-text" autocomplete="off"> <br>
+mobilephonenumber:<input id="mobilephonenumber" type="text" name="mobilephonenumber" value="" class="input-text" autocomplete="off"> <br>
+Message:<input id="message" type="text" name="q2" value="" class="input-text" autocomplete="off"> <br>
+<button id="button44" type="submit" title="For Product">submit your form</button>
+</form>
+
+<p></p><p></p><p></p><p></p><p></p>
+
+<form id="mini_form3" action="#" method="post">
+
+Name: <input id="fullname" type="text" name="captcha-1" value="" class="input-text" autocomplete="off"> <br>
+yourphone:<input id="yourphone" type="text" name="yourphone" value="" class="input-text" autocomplete="off"> <br>
+Message:<input id="message" type="text" name="q2" value="" class="input-text" autocomplete="off"> <br>
+<button id="button44" type="submit" title="For Product">submit your form</button>
+</form>
+
+<p></p><p></p><p></p><p></p><p></p>
+
+<form id="mini_form3" action="#" method="post">
+
+Name: <input id="fullname" type="text" name="captcha-1" value="" class="input-text" autocomplete="off"> <br>
+workphone:<input id="workphone" type="text" name="workphone" value="" class="input-text" autocomplete="off"> <br>
+Message:<input id="message" type="text" name="q2" value="" class="input-text" autocomplete="off"> <br>
+<button id="button44" type="submit" title="For Product">submit your form</button>
+</form>
+
+<p></p><p></p><p></p><p></p><p></p>
+
+<form id="mini_form3" action="#" method="post">
+
+Name: <input id="fullname" type="text" name="captcha-1" value="" class="input-text" autocomplete="off"> <br>
+phonenumber:<input id="phonenumber" type="text" name="phonenumber" value="" class="input-text" autocomplete="off"> <br>
+Message:<input id="message" type="text" name="q2" value="" class="input-text" autocomplete="off"> <br>
+<button id="button44" type="submit" title="For Product">submit your form</button>
+</form>
+
+<p></p><p></p><p></p><p></p><p></p>
+
+<form id="mini_form3" action="#" method="post">
+
+Name: <input id="fullname" type="text" name="captcha-1" value="" class="input-text" autocomplete="off"> <br>
+workphonenumber:<input id="workphonenumber" type="text" name="workphonenumber" value="" class="input-text" autocomplete="off"> <br>
+Message:<input id="message" type="text" name="q2" value="" class="input-text" autocomplete="off"> <br>
+<button id="button44" type="submit" title="For Product">submit your form</button>
+</form>
+
+<p></p><p></p><p></p><p></p><p></p>
+
+<img src="http://reachcodetesting.net/contact-us/red.jpg" id="onclickimage" height=100 width=100>
+
+<img src="http://reachcodetesting.net/contact-us/cake.jpg" id="cake" height=100 width=100>
+
+<h1>Form with XHR Submit Handler</h1>
+  <form action="http://httpbin.org/get" name="form" id="form" method="get">
+    <label for="name">Name</label>
+    <input type="text" name="name" id="name" />
+    <br />
+    <label for="email">Email</label>
+    <input type="text" name="email" id="email" />
+    <br />
+    <label for="phone">Phone</label>
+    <input type ="text" name="phone" id="phone" />
+    <br />
+    <label for="message">Message</label><br />
+    <textarea name="message" id="message"></textarea>
+    <br />
+    <input type="submit" value="JavaScript submit" id="js_submit_form" />
+  </form>
+  <script type="text/javascript">
+    'use strict';
+
+    function submit(e) {
+      if (!window.XMLHttpRequest) {return;}
+
+      e.preventDefault();
+
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', 'http://httpbin.org/get', true);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+          alert('form submitted');
+        }
+      };
+      var data = 'name='+encodeURIComponent(document.getElementById('name').value)+'&' +
+                 'email='+encodeURIComponent(document.getElementById('email').value)+'&' +
+                 'phone='+encodeURIComponent(document.getElementById('phone').value)+'&' +
+                 'message='+encodeURIComponent(document.getElementById('message').value)+'&' +
+                 'js_submit_form='+encodeURIComponent(document.getElementById('js_submit_form').value);
+      xhr.send(data);
+      return false;
+    }
+
+    var form = document.getElementById('form');
+    form.addEventListener('submit', submit);
+  </script>
+  
+  
+</body>
+</html>
+
+
